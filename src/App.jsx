711dfcc8c164484f3689e722d4f405c29f52d7ce
Ian Loron de Almeida <i198933@dac.unicamp.br>
2022-09-27 17:25:58 -0300

    search film

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,56 @@
-import React from 'react'
-import './App.css'
-import Film from './Film'
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+import AddFavourites from './components/AddFavourites';
 
-function App() {
-  return (
-    <div className="App">
-        <Film />
-    </div>
-  )
-}
+const App = () => {
+	const [movies, setMovies] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
+	const [favourites, setFavourites] = useState([]);
+
+	const getMovieRequest = async (searchValue) => {
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+
+		const response = await fetch(url);
+		const responseJson = await response.json();
+
+		if (responseJson.Search) {
+			setMovies(responseJson.Search);
+		}
+	};
+
+	const addFavouriteMovie = (movie) => {
+		const newFavouriteList = [...favourites, movie];
+		setFavourites(newFavouriteList);
+	};
+
+	useEffect(() => {
+		getMovieRequest(searchValue);
+	}, [searchValue]);
+
+	return (
+		<div className='container-fluid movie-app'>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Movies' />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
+			<div className='row'>
+				<MovieList
+					movies={movies}
+					favouriteComponent={AddFavourites}
+					handleFavouritesClick={addFavouriteMovie}
+				/>
+			</div>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Favourites' />
+			</div>
+			<div className='row'>
+				<MovieList movies={favourites} favouriteComponent={AddFavourites} />
+			</div>
+		</div>
+	);
+};
 
 export default App;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 
 const MovieList = (props) => {
-    return (
-        <>
-            {props.movies.map((movie, index) => (
-                <div className='image-container d-flex justify-content-start m-3'>
-                    <img src={movie.Poster} alt='movie'></img>
-                    <div className='overlay d-flex align-items-center justify-content-center'>
-						Add to Favourites
+	const FavouriteComponent = props.favouriteComponent;
+
+	return (
+		<div className="images-container d-flex justify-content-between m-3">
+			{props.movies.map((movie, index) => (
+				<div className='image-container'>
+					<img src={movie.Poster} alt='movie'></img>
+					<div
+						onClick={() => props.handleFavouritesClick(movie)}
+						className='overlay d-flex align-items-center justify-content-center'
+					>
+						<FavouriteComponent />
 					</div>
-                </div>
-            ))}
-        </>
-    );
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default MovieList;
